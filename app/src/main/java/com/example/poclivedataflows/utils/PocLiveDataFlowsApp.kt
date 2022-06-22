0327629525b791cc28a13d7261b433c176141144
Palmira Benavente <palmira.benavente@globant.com>
2022-06-22 18:01:36 -0300

    finished poc

---
+++ app/src/main/java/com/example/poclivedataflows/utils/PocLiveDataFlowsApp.kt
@@ -0,0 +1,19 @@
+package com.example.poclivedataflows.utils
+
+import android.app.Application
+import com.example.poclivedataflows.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class PocLiveDataFlowsApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@PocLiveDataFlowsApp)
+            androidLogger(Level.NONE)
+            modules(listOf(viewModelModule))
+        }
+    }
+}
